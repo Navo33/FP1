4847139aa8534873e1b02b207f3f82b381252122
Ami96-sh <78905947+Ami96-sh@users.noreply.github.com>
2024-07-02 01:04:38 +0530

    Add files via upload

---
+++ test.scala
@@ -0,0 +1,44 @@
+object test extends App {
+
+  //Q1
+  def reverseString(input: String): String = {
+    if (input.isEmpty) ""
+    else reverseString(input.tail) + input.head
+  }
+
+  //Q2
+  def filterStrings(strings: List[String]): List[String] = {
+    strings.filter(_.length > 5)
+  }
+
+  //Q3
+  def arithmeticMean(num1: Int, num2: Int): Double = {
+    val mean = (num1 + num2) / 2.0
+    BigDecimal(mean).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
+
+  //Q4
+  def sumOfEven(numbers: List[Int]): Int = {
+    numbers.filter(_ % 2 == 0).sum
+  }
+
+  val string1 = "String to be reversed"
+  val string2 = reverseString(string1)
+  println(s"\nInput String: $string1")
+  println(s"Reversed String: $string2")
+
+  val inputList = List("name", "christiano", "awesome", "reversed", "rounded", "game")
+  val filteredList = filterStrings(inputList)
+  println(s"\ninput List: $inputList")
+  println(s"Filtered List: $filteredList")
+
+  val num1 = 10
+  val num2 = 15
+  val average = arithmeticMean(num1, num2)
+  println(s"\nArithmetic Mean of $num1 and $num2: $average")
+
+  val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val sumOfEvens = sumOfEven(numbers)
+  println(s"\nInput List: $numbers")
+  println(s"Sum of Even Numbers: $sumOfEvens")
+}
