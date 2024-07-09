904d9c6d3aef7f749d712b0d95042390b47752a0
Ami96-sh <78905947+Ami96-sh@users.noreply.github.com>
2024-07-09 09:41:15 +0530

    Add files via upload

--- q3.scala
+++ q3.scala
@@ -1,12 +1,17 @@
 object q3 extends App {
+  def toUpper(x: String): String = x.toUpperCase()
+
+  def toLower(x: String): String = x.toLowerCase()
+
   def formatNames(name: String)(format: String => String): String = format(name)
 
   val names = List("Benny", "Niroshan", "Saman", "Kumara")
 
   names.foreach {
-    case "Benny" => println(formatNames("Benny")(_.toUpperCase))
+    case "Benny" => println(formatNames("Benny")(toUpper))
     case "Niroshan" => println(formatNames("Niroshan")(name => name.take(2).toUpperCase + name.drop(2).toLowerCase))
-    case "Saman" => println(formatNames("Saman")(_.toLowerCase))
+    case "Saman" => println(formatNames("Saman")(toLower))
     case "Kumara" => println(formatNames("Kumara")(name => name.head.toUpper + name.tail.init.toLowerCase + name.last.toUpper))
   }
 }
+  
